427ff13df7312f5df301a1e22c7e42b1a94ffbb1
deodatojunior <deodatojunior999@gmail.com>
2021-01-09 10:36:51 -0400

    mudanças

--- appva/models.py
+++ appva/models.py
@@ -64,7 +64,7 @@ class Acypr556(models.Model):
     vr_adic_ano_base = models.FloatField(null=True)
     vr_adic_ano_exercicio = models.FloatField(null=True)
     receita_propria = models.FloatField(null=True)
-    populacao = models.models.FloatField(null=True)
+    populacao = models.FloatField(null=True)
     area = models.models.FloatField(null=True)
     remessa = models.CharField(max_length=100, null=True)
     descricao = models.CharField(max_length=500, null=True)
