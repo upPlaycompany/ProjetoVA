4728db0f17857ccadcdaa4c47248496038e63bc1
deodatojunior <deodatojunior999@gmail.com>
2021-02-24 18:02:05 -0400

    a

--- ProjetoVA/settings/base.py
+++ ProjetoVA/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/appva/static/'
+STATIC_URL = '/code/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ProjetoVA/static')
 ]
