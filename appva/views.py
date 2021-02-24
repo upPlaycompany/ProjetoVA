2d1728d6286630a7569e97833418b18008f97dd6
deodatojunior <deodatojunior999@gmail.com>
2021-02-24 16:19:14 -0400

    oksoakosk

--- appva/views.py
+++ appva/views.py
@@ -5153,4 +5153,4 @@ def resultado_simulacao(request, municipio, ano, contribuinte_atual, contribuint
 @login_required
 def RELATORIO_SIMULACAO(request, n):
     nam = [n]
-    return rendering.render_to_pdf_response(request, 'RELATORIO_SIMULACAO.html', {'lista': nam})
+    return rendering.render_to_pdf_response(request, 'RELATORIO_SIMULACAO.html', {'lista': nam}, using='django')
