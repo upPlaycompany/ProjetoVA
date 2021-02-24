dbea137a7d5a71aeaf02065b225dd5bbfa8cded8
deodatojunior <deodatojunior999@gmail.com>
2021-02-24 18:05:15 -0400

    a

--- ProjetoVA/settings/base.py
+++ ProjetoVA/settings/base.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'appva',
     'rest_framework',
     'whitenoise',
-    'django.utils.six',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
