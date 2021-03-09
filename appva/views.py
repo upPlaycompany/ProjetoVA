15a0cb63e3c94d330bacf651b3f5d35bcad151da
deodatojunior <deodatojunior999@gmail.com>
2021-03-09 16:53:25 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -88,7 +88,7 @@ def index(request):
             """SELECT iva_med, iva_75, populacao, ucti, trib_propr, area, coef_soc, ano_exercicio FROM appva_acypr535 WHERE municipio='JUINA' AND remessa='DOE DEFINITIVO';"""
         )
         indices = namedtuplefetchall(cursor)
-    return render(request, 'index.html', {'grafico_evo': grafico_evo_mun,'lista': ranking, 'lista2': indice_par, 'lista3': va_total_estado, 'lista4': indice_medio, 'lista5': distribuicao, 'lista6': finali, 'lista7': indices})
+    return render(request, 'index.html', {'lista': ranking, 'lista2': indice_par, 'lista3': va_total_estado, 'lista4': indice_medio, 'lista5': distribuicao, 'lista6': finali, 'lista7': indices})
 
 
 @login_required
