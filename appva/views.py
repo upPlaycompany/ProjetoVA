fe752a00056d0ea52ac6cc6938641b52631c6670
deodatojunior <deodatojunior999@gmail.com>
2021-03-10 10:12:08 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -51,7 +51,7 @@ def index(request):
         pit.xlabel('Ano de exercício')
         pit.ylabel('Valor adicionado por milhão')
         pit.title('Gráfico de valor adicionado individual - Acorizal')
-        pit.savefig('../static/img/va_mun_evo.png')
+        pit.savefig('va_mun_evo.png')
 
         cursor.execute(
             """SELECT ind_final, ano_exercicio FROM appva_acypr535 WHERE municipio='JUINA' AND remessa='DOE DEFINITIVO';"""
