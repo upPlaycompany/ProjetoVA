31e9ef83b4596cf2a17b8bb51f1beb4c9af7edf4
deodatojunior <deodatojunior999@gmail.com>
2021-03-31 14:08:32 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -977,8 +977,8 @@ def index_barras(request):
             pit.bar(rr5, f_trib, width=0.25)
             pit.bar(rr6, f_area, width=0.25)
             pit.bar(rr7, f_coef, width=0.25)
+            pit.xticks([r + barwidth for r in range(len(f_ano))], f_ano)
             pit.xlabel('Ano de exercício')
-            pit.xticks([r + barwidth for r in range(len(f_ano))], [x for x in f_ano])
             pit.ylabel('Valores em padrão de índice')
             pit.title('Indices do município de Acorizal')
             pit.legend(('Indice médio', '75% do índice', 'Indice população', 'Indice do UCTI', 'Indice Trib. própria',
