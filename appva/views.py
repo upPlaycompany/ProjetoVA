68b891140f1037d621874e07b3dae418f4ecd924
deodatojunior <deodatojunior999@gmail.com>
2021-04-14 14:06:25 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -6797,8 +6797,8 @@ def RELATORIO_VALOR_ADICIONADO_SINTETICO(request, portaria, inscricao, tabela, c
             )
             valor_invalido_entrada = namedtuplefetchall(cursor)
 
-            va = float(str(valor_valido_saida[0].saida_computavel)) - float(
-                str(valor_valido_entrada[0].entrada_computavel))
+            va = [{'valor_adicionado': float(str(valor_valido_saida[0].saida_computavel)) - float(
+                str(valor_valido_entrada[0].entrada_computavel))}]
 
             cursor.execute(
                 """SELECT cfop FROM appva_efd WHERE inscricao=%s AND ano_exercicio=%s GROUP BY cfop;"""
