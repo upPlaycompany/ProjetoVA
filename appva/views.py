b2102fea2d8d1f93474ac3744fb9076f3e118c01
deodatojunior <deodatojunior999@gmail.com>
2021-04-22 11:58:17 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -7378,6 +7378,39 @@ def PRE_RELATORIO_CONTRIBUINTE(request):
                             contabilista='VAZIO', situacao=situacao)
         else:
             pass
+    else:
+        if inscricao:
+            return redirect('RELATORIO_CONTRIBUINTE', tipo_cadastro=tipo_cadastro, inscricao=inscricao,
+                            razao_social='VAZIO',
+                            cpf_cnpj='VAZIO', atividade_economica='VAZIO', tipo_contabilista='VAZIO',
+                            contabilista='VAZIO', situacao='VAZIO')
+        elif razao_social:
+            return redirect('RELATORIO_CONTRIBUINTE', tipo_cadastro=tipo_cadastro, inscricao='VAZIO',
+                            razao_social=razao_social,
+                            cpf_cnpj='VAZIO', atividade_economica='VAZIO', tipo_contabilista='VAZIO',
+                            contabilista='VAZIO', situacao='VAZIO')
+        elif cpf_cnpj:
+            return redirect('RELATORIO_CONTRIBUINTE', tipo_cadastro=tipo_cadastro, inscricao='VAZIO',
+                            razao_social='VAZIO',
+                            cpf_cnpj=cpf_cnpj, atividade_economica='VAZIO', tipo_contabilista='VAZIO',
+                            contabilista='VAZIO', situacao='VAZIO')
+        elif atividade_economica:
+            return redirect('RELATORIO_CONTRIBUINTE', tipo_cadastro=tipo_cadastro, inscricao='VAZIO',
+                            razao_social='VAZIO',
+                            cpf_cnpj='VAZIO', atividade_economica=atividade_economica, tipo_contabilista='VAZIO',
+                            contabilista='VAZIO', situacao='VAZIO')
+        elif contabilista:
+            return redirect('RELATORIO_CONTRIBUINTE', tipo_cadastro=tipo_cadastro, inscricao='VAZIO',
+                            razao_social='VAZIO',
+                            cpf_cnpj='VAZIO', atividade_economica='VAZIO', tipo_contabilista=tipo_contabilista,
+                            contabilista=contabilista, situacao='VAZIO')
+        elif situacao:
+            return redirect('RELATORIO_CONTRIBUINTE', tipo_cadastro=tipo_cadastro, inscricao='VAZIO',
+                            razao_social='VAZIO',
+                            cpf_cnpj='VAZIO', atividade_economica='VAZIO', tipo_contabilista='VAZIO',
+                            contabilista='VAZIO', situacao=situacao)
+        else:
+            pass
     return render(request, 'PRE_RELATORIO_CONTRIBUINTE.html')
 
 
