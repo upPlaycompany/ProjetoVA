34576304ba64f6ded0b596904fc514b521442dd4
deodatojunior <deodatojunior999@gmail.com>
2021-04-22 17:03:46 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -7598,7 +7598,7 @@ def RELATORIO_PTS(request, municipio, tabela):
             resultados = namedtuplefetchall(cursor)
         else:
             cursor.execute(
-                """SELECT * FROM appva_reg_1400_efd WHERE municipio=%s GROUP BY inscricao;""", [municipio]
+                """SELECT * FROM appva_reg_1400_efd WHERE municipio=%s GROUP BY id, inscricao;""", [municipio]
             )
             resultados = namedtuplefetchall(cursor)
     return rendering.render_to_pdf_response(request=request,
