a83bf704de8dfe1bf4eb103b988d4e5401c59b35
deodatojunior <deodatojunior999@gmail.com>
2021-04-22 13:50:21 -0400

    a

--- appva/views.py
+++ appva/views.py
@@ -7418,6 +7418,7 @@ def RELATORIO_CONTRIBUINTE(request, tipo_cadastro, inscricao, razao_social, cpf_
                 )
 
                 resultados = namedtuplefetchall(cursor)
+                float('p')
             elif atividade_economica == 'COMERCIO_INDUSTRIA':
                 codigos = (
                     '4711302', '1011201', '1011205', '3511501', '1013902', '4744099', '4619200', '151201', '4635402',
@@ -7507,7 +7508,6 @@ def RELATORIO_CONTRIBUINTE(request, tipo_cadastro, inscricao, razao_social, cpf_
                 )
 
                 resultados = namedtuplefetchall(cursor)
-                float('p')
             elif atividade_economica == 'COMERCIO_INDUSTRIA':
                 codigos = (
                     '4711302', '1011201', '1011205', '3511501', '1013902', '4744099', '4619200', '151201', '4635402',
